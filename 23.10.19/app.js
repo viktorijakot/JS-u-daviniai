49c58ab736cd2c1b8c90295a33762624f98437d9
viktorija <viktorija.kotaite@gmail.com>
2023-10-24 19:15:32 +0300

    nd

---
+++ 23.10.19/app.js
@@ -0,0 +1,11 @@
+//1a
+document.getElementsByTagName("h1").style.color = "green"
+
+//1b
+const iElem = document.getElementsByTagName("i")
+iElem.className = "small"
+
+//1c
+const h1= document.getElementsByTagName("h1")
+
+console.log(iElem.classList())
